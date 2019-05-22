28baeba14300254c149e0e8c2fd3bd5164e5d31e
Skr00b@!! <carl.yamamoto-furst@bamtechmedia.com>
2019-05-22 18:52:46 -0400

    Dev/send error response (#18)
    * Better error handling on process errors
    
    * better error handling of process errors
    
    * better process error handling

--- server.js
+++ server.js
@@ -11,68 +11,75 @@
 
 // var finto = require("./lib/fintoLib"),
 'use strict';
-var http            = require('http'),
-util                = require('util'),
-config              = require('./lib/config'),
-activeRole          = config.defaultRoleKey,
-ActionLib           = require('./lib/ActionLib'),
-controller          = new ActionLib(),
+var http = require('http'),
+    util = require('util'),
+    config = require('./lib/config'),
+    activeRole = config.defaultRoleKey,
+    ActionLib = require('./lib/ActionLib'),
+    controller = new ActionLib(),
 
-/**
- * Main server set up - basic workflow defined here  with getAction as the main controller method
- * TODO: make an actual controller class or get a frame work to help me do so.
- */
-server = http.createServer(function(req,res) {
-    var
-    
-    postData = '';    
-    console.log("[" + new Date() + "] " + req.url);
-  
-   
+    /**
+     * Main server set up - basic workflow defined here  with getAction as the main controller method
+     * TODO: make an actual controller class or get a frame work to help me do so.
+     */
+    server = http.createServer(function(req, res) {
+        var
+
+            postData = '';
+        console.log("[" + new Date() + "] " + req.url);
+
+
         req.on('data', function(post) {
             postData += post;
             // console.log("got post data: " + postData);
         }).on('end', function() {
             controller.setRequest(req);
             controller.setPostData(postData);
-            try {
-                controller.process(function(err, response) {
-                    if (err) {
-                        
-                        throw(err);
-                    }
-                    res.writeHead(200, {"Content-Type": 'application/json', "Content-length": response.length});
+
+            ///console.log("==== calling process ===");
+            controller.process(function(err, response) {
+                if (err) {
+                    /// console.log("==== got process error ======");
+                    controller.writeError(err, res);
+                } else {
+                    res.writeHead(200, { "Content-Type": 'application/json', "Content-length": response.length });
                     res.write(response);
                     res.end();
-                    });
-              
-            } catch(e) {
-                controller.writeError(e, res);
-            }
+                }
+                // console.log("=== ended process callback ==== ");
+            });
+
+
+            //console.log("==== ended request end event ==== ")
         }).on('error', function(err) {
             controller.writeError(err, res);
+
         });
-        
+
         //TODO: refactor this to be used in the controller.
-        
-}).on('error', function(err) {
-    console.log("Internal Server Error: "+ err);
-    console.error(err);
-});
+
+    }).on('error', function(err) {
+        console.log("Internal Server Error: ");
+        //console.log("==== here =====")
+        console.log(err);
+
+    });
 
 process.on('uncaughtException', function(err) {
+    console.log("==== uncaught exception ===");
+
     server.emit('error', err);
-    
+
 });
 
-process.on('exit', function(code){
-    console.error("===========   exit called!!! Code: " + code +" ===============")
-    
+process.on('exit', function(code) {
+    console.error("===========   exit called!!! Code: " + code + " ===============")
+
 })
 
 server.listen(config.serverSettings, function() {
- 
+
     console.log("server started on..." + config.serverSettings.host + " on port " + config.serverSettings.port);
-    
-   
-} );
+
+
+});
