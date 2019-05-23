6738e80664b1263b397587314740083f1f57aa87
Yamamoto-Furst <carl.yamamoto-furst@bamtechmedia.com>
2019-05-23 15:05:30 -0400

    second expired session token test.

--- test/StsLibTest.js
+++ test/StsLibTest.js
@@ -4,33 +4,39 @@
 
 var StsLib = require('../lib/StsLib'),
 
-stsLib = new StsLib();
+    stsLib = new StsLib();
 
 
 
 console.log("current time:" + Date());
-console.log("adding an hour?");
-var currentTime = new Date().getTime() + 3600*1000,
-newTime = new Date(currentTime);
 
-console.log(newTime);
+var anHourFromNow = new Date().getTime() + 3600 * 1000,
+    newTime = new Date(anHourFromNow),
+    oldCreds,
+    newCreds;
+console.log("an hour from now: " + newTime);
 
 
-stsLib.getCreds('mlb',function(err,creds) {
+
+stsLib.getCreds('xce-bamtech-test', function(err, creds) {
     if (err) {
         console.error(err);
     } else {
         console.log("got credentials: " + creds);
+        oldCreds = creds;
     }
-    
+
 });
 
-setTimeout(function() {stsLib.getCreds('mlb', function(err, creds){
-    
-    if (err) {
-        console.error(err);
-    } else {
-        console.log("got creds after an hour: " + creds);
-    }
-   }
-)},3600 * 1000 + 10); // 10 seconds after the hour
+setTimeout(function() {
+    stsLib.getCreds('xce-bamtech-test', function(err, creds) {
+
+        if (err) {
+            console.error(err);
+        } else {
+            console.log("got creds after an hour: " + creds)
+            newCreds;
+            assert(newCreds !== oldCreds);
+        }
+    })
+}, 3600 * 1000 + 60); // 1  minute after the hour
