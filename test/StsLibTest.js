8117d9377cda94ada3abd03d50374b0a053f7869
Skr00b@!! <carl.furst@seefurst.com>
2020-01-10 11:32:44 -0500

    Dev/cfurst/ce 375 (#19)
    * Better error handling on process errors
    
    * better error handling of process errors
    
    * better process error handling
    
    * second expired session token test.
    
    * Created a test for stale tokens
    
    *  reverted StsLibTest.js to testing StsLib
    
    * reverted tests and corrected timeout timing.
    
    * test confirming we are using the same token after an hour.

--- test/StsLibTest.js
+++ test/StsLibTest.js
@@ -3,34 +3,40 @@
  */
 
 var StsLib = require('../lib/StsLib'),
-
+    assert = require('assert');
 stsLib = new StsLib();
 
 
 
 console.log("current time:" + Date());
-console.log("adding an hour?");
-var currentTime = new Date().getTime() + 3600*1000,
-newTime = new Date(currentTime);
 
-console.log(newTime);
+var anHourFromNow = new Date().getTime() + 3600 * 1000,
+    newTime = new Date(anHourFromNow),
+    oldCreds;
+console.log("an hour from now: " + newTime);
 
 
-stsLib.getCreds('mlb',function(err,creds) {
+
+stsLib.getCreds('xce-bamtech-test', function(err, creds) {
     if (err) {
         console.error(err);
     } else {
         console.log("got credentials: " + creds);
+        oldCreds = creds;
     }
-    
+
 });
 
-setTimeout(function() {stsLib.getCreds('mlb', function(err, creds){
-    
-    if (err) {
-        console.error(err);
-    } else {
-        console.log("got creds after an hour: " + creds);
-    }
-   }
-)},3600 * 1000 + 10); // 10 seconds after the hour
+setTimeout(function() {
+    stsLib.getCreds('xce-bamtech-test', function(err, creds) {
+
+        if (err) {
+            console.error(err);
+        } else {
+            console.log("got creds after an hour. we are comparing:");
+            console.log(`${creds.Token} with`);
+            console.log(`${oldCreds.Token}`);
+            assert(creds.Token !== oldCreds.Token);
+        }
+    })
+}, 3600 * 1000 + 60 * 1000); // 1  minute after the hour in ms
